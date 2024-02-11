69646ba67e66b614153a8a9bef8931be1a975b86
lucille <lucille.cenac@ynov.com>
2024-02-11 15:30:43 +0100

    add model php

---
+++ Front/Templates/modele1.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../Css/model1.css">
+    <meta charset="UTF-8">
+    <title>Curriculum Vitae</title>
+</head>
+<body>
+
+<div class="title">
+    <?php
+    include '../../server/information.php';
+
+    global $bdd;
+    $user = $bdd->prepare("SELECT * FROM users WHERE User_id=?");
+    $user->execute([GetUserID()]);
+
+    while ($row = $user->fetch(PDO::FETCH_ASSOC)) {
+        echo "<img>";
+        echo "<h1>" . strtoupper($row['Last_name']) . " " . $row['First_name'] . "</h1>";
+    }
+    ?>
+</div>
+
+<section>
+    <div>
+        <h2>Profil</h2>
+        <ul>
+            <?php
+            global $bdd;
+            $user = $bdd->prepare("SELECT * FROM users WHERE User_id=?");
+            $user->execute([GetUserID()]);
+            while ($row = $user->fetch(PDO::FETCH_ASSOC)) {
+                echo "<li><strong>Téléphone: </strong>" . $row['Phone_tel'] ."</li>";
+                echo "<li><strong>Email: </strong>" . $row['Email'] ."</li>";
+            }
+            ?>
+        </ul>
+    </div>
+</section>
+
+<section>
+    <h2>Parcours Académique</h2>
+    <?php
+
+    while ($row = $academics->fetch(PDO::FETCH_ASSOC)) {
+        echo "<h3>" . $row['Diploma'] ."</h3>";
+        echo "<p><strong>Établissement: </strong>" . $row['School'] ."</p>";
+        echo "<p><strong>Date: </strong>". $row['Date_start'] . $row['Date_end'] ."</p>";
+    }
+    ?>
+</section>
+
+<section>
+    <h2>Expérience Professionnelle</h2>
+    <?php
+
+    while ($row = $experiences->fetch(PDO::FETCH_ASSOC)) {
+        echo "<h3>" . $row['Job'] ."</h3>";
+        echo "<p><strong>Entreprise: </strong>". $row['Company'] ."</p>";
+        echo "<p><strong>Date: </strong>". $row['Date_start'] . $row['Date_end'] ."</p>";
+        echo "<p><strong>Description: </strong>". $row['Descriptions'] ."</p>";
+    }
+    ?>
+</section>
+
+<section>
+    <h2>Loisir</h2>
+    <?php
+    global $bdd;
+    $cv = $bdd->prepare("SELECT * FROM cv WHERE User_id=?");   //TODO id du cv
+    $cv->execute([GetUserID()]);
+
+    while ($row = $cv->fetch(PDO::FETCH_ASSOC)) {
+        echo "<p>". $row['Hobbies'] ."</p>";
+    }
+    ?>
+</section>
+
+</body>
+</html>
+
---
+++ Front/Templates/modele2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../Css/model2.css">
+    <meta charset="UTF-8">
+    <title>Curriculum Vitae</title>
+    <style>
+
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <div class="sidebar">
+        <?php
+        include '../../server/information.php';
+
+        global $bdd;
+        $user = $bdd->prepare("SELECT * FROM users WHERE User_id=?");
+        $user->execute([GetUserID()]);
+
+        while ($row = $user->fetch(PDO::FETCH_ASSOC)) {
+            echo '<img src="votre-photo.jpg" alt="Votre Photo" width="150" height="150" style="border-radius: 50%;">';
+            echo "<h1 style='color: white'>". strtoupper($row['Last_name'])." ". $row['First_name'] ."</h1>";
+            echo "<h2>Profil</h2>";
+            echo "<ul>";
+            echo "<li><strong>Téléphone: </strong>". $row['Phone_tel'] ."</li>";
+            echo "<li><strong>Email: </strong>". $row['Email'] ."</li>";
+            echo "</ul>";
+        }
+        ?>
+    </div>
+
+    <div class="content">
+        <h2>Parcours Académique</h2>
+        <?php
+        while ($row = $academics->fetch(PDO::FETCH_ASSOC)) {
+            echo "<h3>".$row['Diploma']."</h3>";
+            echo "<p><strong>Établissement: </strong>". $row['School'] ."</p>";
+            echo "<p><strong>Date: </strong>". $row['Date_start']. $row['Date_end'] ."</p>";
+        }
+        ?>
+
+        <h2>Expérience Professionnelle</h2>
+        <?php
+        while ($row = $experiences->fetch(PDO::FETCH_ASSOC)) {
+            echo "<h3>". $row['Job'] ."</h3>";
+            echo "<p><strong>Entreprise : </strong>". $row['Company'] ."</p>";
+            echo "<p><strong>Date : </strong>". $row['Date_start']. $row['Date_end'] ."</p>";
+            echo "<p><strong>Description : </strong>". $row['Descriptions'] ."</p>";
+        }
+        ?>
+
+        <h2>Loisir</h2>
+        <?php
+        global $bdd;
+        $cv = $bdd->prepare("SELECT * FROM cv WHERE User_id=?");   //TODO id du cv
+        $cv->execute([GetUserID()]);
+
+        while ($row = $cv->fetch(PDO::FETCH_ASSOC)) {
+            echo "<p>". $row['Hobbies'] ."</p>";
+        }
+        ?>
+
+    </div>
+</div>
+
+</body>
+</html>
